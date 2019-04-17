6153b0f3313c1b461e28da08f094cb43ea6b53d3
Terrence_0_163 <Terrence_0_163@163.com>
2019-04-17 13:32:54 +0800

    Initial commit

---
+++ src/main/java/com/tao/knowledgetree/KnowledgetreeApplication.java
@@ -0,0 +1,13 @@
+package com.tao.knowledgetree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnowledgetreeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KnowledgetreeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tao/knowledgetree/KnowledgetreeApplicationTests.java
@@ -0,0 +1,16 @@
+package com.tao.knowledgetree;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class KnowledgetreeApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
